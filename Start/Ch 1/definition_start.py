d56e1731c65c5b122cd4b926d2180d543fdc0064
mtp3b <138579970+mtp3b@users.noreply.github.com>
2024-05-20 17:33:10 +0000

    05212024_0132H

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,15 @@
 
 
 # TODO: create a basic class
-
+class Book:
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
+book1 = Book("Onslaught")
+book2 = Book("Legions")
 
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -5,21 +5,39 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.__secret = "SECRET ATTRIBUTE"
+
         # TODO: add properties
 
     # TODO: create instance methods
+    def getprice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+
+    def setdiscount(self, amount):
+        self._discount = amount  #_ hints to other developers that this attribute is considered internal to the class
+    
 
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+b2 = Book("Legions", "Phage the Untouchable", 400, 12.25)
 
 # TODO: print the price of book1
-
+print(b1.getprice())
 
 # TODO: try setting the discount
+print(b2.getprice())
+b1.setdiscount(0.5)
+print(b1.getprice())
 
 
 # TODO: properties with double underscores are hidden by the interpreter
+print(b2.__secret)
